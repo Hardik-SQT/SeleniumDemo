906a9f75367a8e90a2892a780eb73bc60e0e8ae2
Hardik Kalavadiya <hardikkalavadiya88@gmail.com>
2023-12-26 18:41:02 +0530

    New class file addition in code

---
+++ src/test/java/testNgAnnotations/Demo5.java
@@ -0,0 +1,11 @@
+package testNgAnnotations;
+
+import org.testng.annotations.Test;
+
+public class Demo5 {
+
+	@Test
+	public void gitDemo() {
+		System.out.println("This is git demo class for testing purpose only.");
+	}
+}
