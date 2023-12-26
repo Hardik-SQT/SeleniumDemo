d9b5a7dce288e6b7759701f1d7755790ba9e8f55
Hardik Kalavadiya <hardikkalavadiya88@gmail.com>
2023-12-26 18:45:37 +0530

    New class Demo7 added

---
+++ src/test/java/testNgAnnotations/Demo7.java
@@ -0,0 +1,11 @@
+package testNgAnnotations;
+
+import org.testng.annotations.Test;
+
+public class Demo7 {
+
+	@Test
+	public void gitDemo7() {
+		System.out.println("This is test git demo 7 class");
+	}
+}
